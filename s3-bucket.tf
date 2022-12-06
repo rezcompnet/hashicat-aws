4b6a9f257f6bfdd01a7f239b0207517061d484f6
reza <reza.pratama@compnet.co.id>
2022-12-06 07:25:39 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3_bucket" {
+  source  = "app.terraform.io/testrezcompnet/s3-bucket/aws"
+  version = "2.8.0"
+
+  bucket_prefix = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
